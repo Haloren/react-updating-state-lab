7394676258e8ee8e3e8d5485ec08b796c45b3d02
Haloren <mckennakyler@gmail.com>
2021-02-25 01:44:35 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,53 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(count => {
+          return {
+            timesClicked: count.timesClicked + 1
+          }
+        })
+      }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+}
+
+
+// //BUILDING AS A FUNCTIONAL COMPONENT
+
+// import React, {useState} from 'react';
+
+// export default function DigitalClicker() {
+
+//     const [count, timesClicked] = useState(0);
+
+//     const handleClick = () => {
+//         timesClicked(count => count + 1);
+//     }
+
+//     const styleObj = {
+//         background: "#264653",
+//         color: "#F3FFE1",
+//         fontSize: "4rem",
+//         boxShadow: "2px 2px 20px #000",
+//         margin: "10px",
+//         padding: "10px 20px"
+//     }
+
+//     return(
+//         // <button onClick={ () => timesClicked(count + 1) }>{count}</button>
+//         <button onClick={ handleClick } style={styleObj} >{count}</button>
+//     )
+// }
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,93 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+    }
+
+    bitrateHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+       
+    }
+
+    resolutionHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.bitrateHandler}>Bitrate: {this.state.settings.bitrate} </button>
+                <button className='resolution' onClick={this.resolutionHandler}>Resolution: {this.state.settings.video.resolution} </button>
+            </div>
+        )
+    }
+}
+
+
+// //BUILDING AS A FUNCTIONAL COMPONENT
+
+// import React, {useState} from "react";
+
+// export default function YouTubeDebugger() {
+
+//     const errors = useState()
+//     const user = useState(null)
+//     const settings = {
+//         bitrate: 8, //const [bitrate, setBitrate] = useState("8");
+//         video: { 
+//             resolution: '720p' //const [resolution, setResolution] = useState("1080p");
+//         }
+//     }
+
+//     const resolutionHandler = () => {
+//         // console.log("resolution")
+//         setResolution(resolution => resolution === "1080p" ? resolution = "720p" : "1080p")
+//     }
+
+//     const bitrateHandler = () => {
+//         // console.log("bitrate")
+//         setBitrate(bitrate => bitrate === "8" ? bitrate = "12" : bitrate = "8")
+//     }
+
+//     const styleObj = {
+//         background: "#2a9d8f",
+//         color: "#F3FFE1",
+//         fontSize: "2rem",
+//         boxShadow: "2px 2px 20px #000",
+//         margin: "10px",
+//         padding: "10px 20px"
+//     }
+
+//     return(
+//         <div>
+//             <button className="bitrate" onClick={bitrateHandler} style={styleObj} >Bitrate: {bitrate} </button>
+//             <br></br>
+//             <button className="resolution" onClick={resolutionHandler} style={styleObj} >Resolution: {resolution} </button>
+//         </div>
+//     )
+// }
